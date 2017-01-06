c4e928e9cc122a2412434315486381826a57e753
spacedebrix <spacedebrix@gmail.com>
2017-01-05 21:49:23 -0500

    Removing unused function

--- app/src/main/java/com/spacedebrix/entertainment/encrgr/MainActivity.java
+++ app/src/main/java/com/spacedebrix/entertainment/encrgr/MainActivity.java
@@ -98,12 +98,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
-
-    }
-
-    @Override
     protected void onResume() {
         super.onResume();
 
